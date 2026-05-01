355b96375f8d9d3330c5e0da548ab7d472f9a6e4
Станислав <kirkizhsa@mail.ru>
2026-05-02 00:13:09 +0500

    на сервер12

--- beckend/znak/settings.py
+++ beckend/znak/settings.py
@@ -165,10 +165,10 @@ CORS_ALLOW_HEADERS = [
 ]
 
 # ========== HTTPS НАСТРОЙКИ (только в продакшене) ==========
-# if os.getenv('PRODUCTION', 'False') == 'True':
-#     SECURE_SSL_REDIRECT = True
-#     SESSION_COOKIE_SECURE = True
-#     CSRF_COOKIE_SECURE = True
+if os.getenv('PRODUCTION', 'False') == 'True':
+     SECURE_SSL_REDIRECT = True
+     SESSION_COOKIE_SECURE = True
+     CSRF_COOKIE_SECURE = True
 #     SECURE_BROWSER_XSS_FILTER = True
 #     SECURE_CONTENT_TYPE_NOSNIFF = True
 #     SECURE_HSTS_SECONDS = 31536000
